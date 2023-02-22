55429856aaa61dca523b74fca8dcdebdb0831f8e
Marika <mbomjour@gmail.com>
2023-02-22 03:33:30 +0300

    Parameterized test

--- src/test/java/VacationServiceTest.java
+++ src/test/java/VacationServiceTest.java
@@ -1,25 +1,30 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationServiceTest {
 
-    @Test
-    public void shouldCalcVacation1() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/vacation.csv")
+
+    public void shouldCalcVacation(int expected, int income, int expense, int threshold) {
         VacationService service = new VacationService();
 
-        int expected = 3;
-        int actual = service.calcVacation(10_000, 3_000, 20_000);
+//        int expected = 3;
+        int actual = service.calcVacation(income, expense, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void shouldCalcVacation2() {
-        VacationService service = new VacationService();
-
-        int expected = 2;
-        int actual = service.calcVacation(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
+//    @Test
+//    public void shouldCalcVacation2() {
+//        VacationService service = new VacationService();
+//
+//        int expected = 2;
+//        int actual = service.calcVacation(100_000, 60_000, 150_000);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
